2dd0d2b2ded462c92158b398efb40195da8b9751
LovishPrajapati <45449591+LovishPrajapati@users.noreply.github.com>
2021-02-12 21:10:36 +0530

    Update one.js

--- src/one.js
+++ src/one.js
@@ -1,26 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const One= (props) => {
-const[num1,setNum1] = useState("");
-const incNum1=()=> { 
-    if(num1< 10){
-    setNum1(num1+1);
-}else{
-    setNum1(10);
-}}
-const decNum1=()=> {
-    if(num1 > 0){
-    setNum1(num1-1);
-}else{
-    setNum1(0);
-}}
-return(
-
-<div>
-<button onClick={decNum1}>-</button>
-<h1>{num1}</h1>
-<button onClick={incNum1}>+</button>
- </div>
-);
-}
+const One = ({ num }) => {
+	const [num1, setNum1] = useState(num);
+	const incNum1 = () => {
+		setNum1((prev) => prev + 1);
+	};
+	const decNum1 = () => {
+		setNum1((prev) => prev - 1);
+	};
+	return (
+		<div>
+			<button onClick={decNum1} disabled={!num1}>
+				-
+			</button>
+			<h1>{num1}</h1>
+			<button onClick={incNum1}>+</button>
+		</div>
+	);
+};
 export default One;
